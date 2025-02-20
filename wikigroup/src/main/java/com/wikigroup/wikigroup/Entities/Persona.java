617520747013c8ae63835a3071e87e2b1cdcf344
Carlosmercg <carlosdavidmercadogallego@gmail.com>
2025-02-20 13:27:55 -0500

    Correccion preliminar de entities y repositories

--- wikigroup/src/main/java/com/wikigroup/wikigroup/Entities/Persona.java
+++ wikigroup/src/main/java/com/wikigroup/wikigroup/Entities/Persona.java
@@ -1,10 +1,9 @@
 package com.wikigroup.wikigroup.Entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,4 +21,7 @@ public class Persona {
     private String email;
     private String mensaje;
 }
+
+
+
 
--- wikigroup/src/main/java/com/wikigroup/wikigroup/Repositories/PersonaRepository.java
+++ wikigroup/src/main/java/com/wikigroup/wikigroup/Repositories/PersonaRepository.java
@@ -1,8 +1,11 @@
 package com.wikigroup.wikigroup.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.wikigroup.wikigroup.Entities.Persona;
 
+@Repository
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
 }
 
